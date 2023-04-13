17b1c972daea211118b6fe6bb5508455a3c9658e
santi760 <58080648+santi760@users.noreply.github.com>
2023-04-12 21:31:37 -0300

    Clases

--- src/main/java/tendenciasMusicales/Popularidad.java
+++ src/main/java/tendenciasMusicales/Popularidad.java
@@ -1,5 +1,5 @@
 package tendenciasMusicales;
 public abstract class Popularidad {//se le delega la reproduccion a popularidad porque puedo saber como moverme de popularidad
-detalleCompletoPara();
-reproducir();
+//detalleCompletoPara();
+//reproducir();
 }
